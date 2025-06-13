30cef86b322d480ef773dff6c9d153bc69fc9e3e
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2025-06-13 07:38:04 +0545

    purchase and sales

--- ims/models.py
+++ ims/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=300)
     username = models.CharField(max_length=300)
-    phone_no = models.IntegerField(null=True)
+    phone_no = models.IntegerField(max_length=10, null=True)
     address = models.CharField(max_length=300,null=True)
 
 class Product(models.Model):
@@ -17,3 +17,15 @@ class Supplier(models.Model):
     contact = models.IntegerField(max_length=10)
     address = models.CharField(max_length=300)
     email = models.EmailField()
+
+class Purchase(models.Model):
+    quantity = models.IntegerField()
+    price  = models.IntegerField()
+    product = models.ForeignKey(Product)
+    supplier = models.ForeignKey(Supplier)
+
+class Sale(models.Model):
+    quantity = models.IntegerField()
+    price  = models.IntegerField()
+    product = models.ForeignKey(Product)
+    supplier = models.ForeignKey(Supplier)
